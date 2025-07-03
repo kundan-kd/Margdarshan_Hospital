87e263544dfda451e51d429f5e6a0b37c66f12c6
kundan-kd <kundan.techiesquad@gmail.com>
2025-07-03 19:36:46 +0530

    priority in db

--- database/migrations/2025_06_18_154639_create_bed_groups_table.php
+++ database/migrations/2025_06_18_154639_create_bed_groups_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('bed_groups', function (Blueprint $table) {
             $table->id();
             $table->string('name',50)->nullable();
+            $table->integer('priority')->nullable();
             $table->integer('status')->default(1);
             $table->timestamps();
             $table->softDeletes();
